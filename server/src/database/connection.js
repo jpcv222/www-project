c74c48ad3bc19b305e40339941a051ad20faa60b
jeffrey2423 <riosjeff24@gmail.com>
2020-10-06 21:46:33 -0500

    correcciones generales

--- server/src/database/connection.js
+++ server/src/database/connection.js
@@ -5,4 +5,18 @@ const connection = new Pool(
     config.DB_CREDENTIALS.HEROKU
 );
 
+connection.connect(async (err, client, done) => {
+    try {
+        if (err) {
+            console.log(err.message);
+        } else {
+            console.log("Conexion a base de datos exitosa");
+        }
+    }catch(err){
+        console.log(err.message);
+    }finally{
+        done();
+    }
+});
+
 module.exports = connection;
