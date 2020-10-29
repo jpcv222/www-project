f0377de2122616e6365b7ab32aa739c36157d714
jeffrey2423 <riosjeff24@gmail.com>
2020-10-29 16:18:32 -0500

    nueva configuracion agregada

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,5 +1,6 @@
 module.exports = {
     SOCKET_SERVER_URI: "//localhost:4000",
     QUERY_SERVER_URI: "http://localhost:4000/",
+    // QUERY_SERVER_URI: "http://192.168.1.10:4000/", 
     ENCRYPTION_SECRET_KEY:  "www-project",
 }
