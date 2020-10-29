c43e78deea2b016ee5351fbc9c0b24d50256fda7
jeffrey2423 <riosjeff24@gmail.com>
2020-10-29 16:27:20 -0500

    Mejora en el controlador para la autenticacion

--- server/src/controllers/auth_controller.js
+++ server/src/controllers/auth_controller.js
@@ -7,66 +7,70 @@ const jwt = require("jsonwebtoken");
 authController.auth = async (req, res) => {
     try {
         let io = req.app.get('socketIo');
-        let query = {
-            text: "select * from f_validate_auth($1)",
-            values: [req.body]
-        };
+        if (resources_controller.validarIdentification(req.body.identification)) {
+            let query = {
+                text: "select * from f_validate_auth($1)",
+                values: [req.body]
+            };
+            await connection.connect(async (err, client, done) => {
+                try {
+                    if (err) {
+                        res.json(resources_controller.leerRecurso(1005, err.message));
+                    } else {
+                        await client.query(query, async (err, results) => {
+                            if (err) {
+                                await client.query("ROLLBACK");
+                                res.json(resources_controller.leerRecurso(1005, err.message));
+                            } else {
+                                switch (results.rows[0].f_validate_auth) {
+                                    case resources_controller.ESTADO_USUARIO.NO_EXISTE:
+                                        res.json(resources_controller.leerRecurso(1008));
+                                        break;
+                                    case resources_controller.ESTADO_USUARIO.INACTIVO:
+                                        res.json(resources_controller.leerRecurso(1007));
+                                        break;
+                                    case resources_controller.ESTADO_USUARIO.DATOS_INCORRECTOS:
+                                        res.json(resources_controller.leerRecurso(1006));
+                                        break;
+                                    default:
+                                        query = {
+                                            text: "select * from f_user_auth($1)",
+                                            values: [req.body]
+                                        };
+                                        await client.query(query, async (err, results) => {
+                                            if (!err) {
+                                                jwt.sign(results.rows[0], CONFIG.ENCRYPTION_SECRET_KEY, (jwtErr, token) => {
+                                                    if (!jwtErr) {
+                                                        res.status(200).json({
+                                                            token: token
+                                                        });
+                                                    } else {
+                                                        res.json(resources_controller.leerRecurso(1005, jwtErr.message));
+                                                    }
+                                                });
 
-        await connection.connect(async (err, client, done) => {
-            try {
-                if (err) {
-                    res.json(resources_controller.leerRecurso(1005, err.message));
-                } else {
-                    await client.query(query, async (err, results) => {
-                        if (err) {
-                            await client.query("ROLLBACK");
-                            res.json(resources_controller.leerRecurso(1005, err.message));
-                        } else {
-                            switch (results.rows[0].f_validate_auth) {
-                                case resources_controller.ESTADO_USUARIO.NO_EXISTE:
-                                    res.json(resources_controller.leerRecurso(1008));
-                                    break;
-                                case resources_controller.ESTADO_USUARIO.INACTIVO:
-                                    res.json(resources_controller.leerRecurso(1007));
-                                    break;
-                                case resources_controller.ESTADO_USUARIO.DATOS_INCORRECTOS:
-                                    res.json(resources_controller.leerRecurso(1006));
-                                    break;
-                                default:
-                                    query = {
-                                        text: "select * from f_user_auth($1)",
-                                        values: [req.body]
-                                    };
-                                    await client.query(query, async (err, results) => {
-                                        if (!err) {
-                                            jwt.sign(results.rows[0], CONFIG.ENCRYPTION_SECRET_KEY, (jwtErr, token) => {
-                                                if (!jwtErr) {
-                                                    res.status(200).json({
-                                                        token: token
-                                                    });
-                                                } else {
-                                                    res.json(resources_controller.leerRecurso(1005, jwtErr.message));
-                                                }
-                                            });
-
-                                        } else {
-                                            await client.query('ROLLBACK');
-                                            res.json(resources_controller.leerRecurso(1005, err.message));
-                                        }
-                                    });
+                                            } else {
+                                                await client.query('ROLLBACK');
+                                                res.json(resources_controller.leerRecurso(1005, err.message));
+                                            }
+                                        });
+                                }
                             }
-                        }
-                    });
+                        });
+                    }
+                } finally {
+                    done();
+                    query = {};
+                    io = "";
                 }
-            } finally {
-                done();
-                query = {};
-                io = "";
-            }
-        });
+            });
+        } else {
+            res.json(resources_controller.leerRecurso(1013));
+        }
     } catch (error) {
         res.json(resources_controller.leerRecurso(1005, error.message));
     }
+
 }
 
 
