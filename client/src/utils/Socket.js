f0dd1508bf60dea467b7386a9341ecb110156c60
jeffrey2423 <riosjeff24@gmail.com>
2020-11-01 15:47:01 -0500

    mejoras generales

--- client/src/utils/Socket.js
+++ client/src/utils/Socket.js
@@ -1,6 +1,12 @@
 import io from 'socket.io-client';
-import config from '../config/config'
+import config from '../config/config';
+import validations from '../resources/validations/main'
+let socket
+try {
+    socket = io(config.SOCKET_SERVER_URI);
+} catch (error) {
+    validations.ErrorToast("Ha ocurrido un error", error.message)
+}
 
-let socket = io(config.SOCKET_SERVER_URI);
 
 export default socket;
