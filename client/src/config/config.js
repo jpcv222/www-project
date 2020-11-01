ea841cb7b42245eb1da48398252cdd31dbcc3f9a
jeffrey2423 <riosjeff24@gmail.com>
2020-11-01 15:46:45 -0500

    mejoras generales

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,6 +1,7 @@
 module.exports = {
     SOCKET_SERVER_URI: "//localhost:4000",
+    // SOCKET_SERVER_URI: "//192.168.1.11:4000",
     QUERY_SERVER_URI: "http://localhost:4000/",
-    // QUERY_SERVER_URI: "http://192.168.1.10:4000/", 
+    // QUERY_SERVER_URI: "http://192.168.1.11:4000/", 
     ENCRYPTION_SECRET_KEY:  "www-project",
 }
