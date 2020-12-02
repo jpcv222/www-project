abff7974d94c1372241f62b26071c84f2801416f
jeffrey2423 <riosjeff24@gmail.com>
2020-12-02 18:34:08 -0500

    Correccion en vista para obtener los logs de login del usuario

--- server/src/database/views/v_users_logs.sql
+++ server/src/database/views/v_users_logs.sql
@@ -1,11 +1,13 @@
 CREATE OR REPLACE VIEW v_users_logs AS(
-    SELECT 
-        LOGS.ts_creation,       
-        USERS.identification,
-        USERS.name,
-        USERS.lastname,
-        LOGS.action
-        FROM LOGS
-        LEFT JOIN USERS
-        ON USERS.row_id = LOGS.user_id
+    SELECT   
+		MAX(LOGS.ts_creation) last_login,
+		COUNT(USERS.identification) login_count,
+		USERS.identification,
+		USERS.name,
+		USERS.lastname
+		FROM LOGS
+		LEFT JOIN USERS
+		ON USERS.row_id = LOGS.user_id
+	WHERE EXTRACT(MONTH FROM LOGS.ts_creation) = EXTRACT(MONTH FROM NOW())
+	GROUP BY USERS.identification, USERS.name, USERS.lastname
 );
